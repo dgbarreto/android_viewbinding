3f051ca5aa12aa2c9f5d3b6b24f6b4566cd45afc
Danilo Barreto <danilo.g.barreto@avanade.com>
2021-08-23 10:51:49 -0300

    Solution

--- app/src/main/java/com/avanade/viewbinding/MainActivity.kt
+++ app/src/main/java/com/avanade/viewbinding/MainActivity.kt
@@ -2,10 +2,29 @@ package com.avanade.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.avanade.viewbinding.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        //Antes
+        val oldButton: Button = findViewById<Button>(R.id.myButton)
+        val myText: TextView = findViewById<TextView>(R.id.myTextView)
+
+        //Agora
+        val button: Button = binding.myButton
+        val text: TextView = binding.myTextView
+
+        binding.myTextView.text = "new text"
     }
+
+
 }
